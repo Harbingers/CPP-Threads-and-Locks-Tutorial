5a4ef7603166dc0781519aaaaa081e08250ee3e7
Harbingers <financesmacro@gmail.com>
2016-04-18 17:26:55 -0400

    Add a few threads

--- locks_for_counter.cpp
+++ locks_for_counter.cpp
@@ -8,3 +8,24 @@ struct Counter {
         ++value;
     }
 };
+
+int main(){
+    Counter counter;
+
+    std::vector<std::thread> threads;
+    for(int i = 0; i < 5; ++i){
+        threads.push_back(std::thread([&counter](){
+            for(int i = 0; i < 100; ++i){
+                counter.increment();
+            }
+        }));
+    }
+
+    for(auto& thread : threads){
+        thread.join();
+    }
+
+    std::cout << counter.value << std::endl;
+
+    return 0;
+}
