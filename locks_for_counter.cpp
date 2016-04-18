1ff5c2e53de1804073b39f096da60b2c02eae042
Harbingers <financesmacro@gmail.com>
2016-04-18 17:26:20 -0400

    Update locks_for_counter.cpp

--- locks_for_counter.cpp
+++ locks_for_counter.cpp
@@ -1,2 +1,10 @@
-//
+// A Thread-Safe Counter 
 // Reference: http://baptiste-wicht.com/posts/2012/03/cp11-concurrency-tutorial-part-2-protect-shared-data.html
+
+struct Counter {
+    int value;
+    Counter() : value(0){}
+    void increment(){
+        ++value;
+    }
+};
