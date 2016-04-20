3985759b3fbab226b09e834a81d0ee863c7b2675
Harbingers <financesmacro@gmail.com>
2016-04-20 15:13:23 -0400

    Update and rename locks_for_counter.cpp to mutex_for_counter.cpp

--- mutex_for_counter.cpp
+++ mutex_for_counter.cpp
@@ -1,25 +1,39 @@
-// A Thread-Safe Counter 
+// A Thread-Safe Counter with Mutex
 // Reference: http://baptiste-wicht.com/posts/2012/03/cp11-concurrency-tutorial-part-2-protect-shared-data.html
 
-/**
- * Problem Without Locks:
- * 1) incrementation is not an atomic operation. It is made of three operations:
+/*
+ * 1. Problem Without Locks:
+ * incrementation is not an atomic operation. It is made of three operations:
  * - Read the current value of value
  * - Add one to the current value
  * - Write that new value to value
-
+ *
+ * 2. Use a mutex to make counter thread-safe
+ * 3. Consider exception: need to unlock() in a try-catch block, otherwise, will never unlock()
 */
 
 #include <thread>
 #include <iostream>
 #include <vector>
+#include <mutex>
 
 // a counter, increase 1 whenever increment() is called
 struct Counter {
+    std::mutex mutex;
     int value;
-    Counter() : value(0){}
+
+    Counter() : value(0) {}
+
     void increment(){
+        mutex.lock();
         ++value;
+        mutex.unlock();
+    }
+
+    void decrement(){
+        mutex.lock();
+        --value;
+        mutex.unlock();
     }
 };
 
@@ -27,8 +41,9 @@ int main(){
     Counter counter;
 
     std::vector<std::thread> threads;
+
     for(int i = 0; i < 5; ++i){
-        threads.push_back(std::thread([&counter](){
+        threads.push_back(std::thread([&](){
             for(int i = 0; i < 100; ++i){
                 counter.increment();
             }
@@ -44,7 +59,9 @@ int main(){
     return 0;
 }
 
-/**
+/*
+ * Compile: clang++ -std=c++11 mutex_for_counter.cpp -pthread
+ *
  * Output:
  * 500
- * /
+ */
