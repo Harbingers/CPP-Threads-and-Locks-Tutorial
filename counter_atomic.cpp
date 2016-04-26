eda4cc2aea3f9e2959633404ff0679b5b00ac827
Harbingers <financesmacro@gmail.com>
2016-04-26 17:19:33 -0400

    Update counter_atomic.cpp

--- counter_atomic.cpp
+++ counter_atomic.cpp
@@ -1,9 +1,20 @@
+// An Atomic Counter
+// Reference: http://baptiste-wicht.com/posts/2012/07/c11-concurrency-tutorial-part-4-atomic-type.html
+
 //=======================================================================
 // Copyright (c) 2014 Baptiste Wicht
 // Distributed under the terms of the MIT License.
 // (See accompanying file LICENSE or copy at
 //  http://opensource.org/licenses/MIT)
 //=======================================================================
+
+/*
+ 1. std::atomic, you can use any type with that template and the ops on that variable, it will be atomic (thread safe)
+
+ 2. It is up to the lib implementation to choose which synchronization is used to make atomic.
+    iF it is int. long. float, it will be lock-free technique, good
+    if it is big type, mutexes will be used, no performance advantage
+ */
 
 #include <thread>
 #include <atomic>
@@ -48,3 +59,10 @@ int main(){
 
     return 0;
 }
+
+/*
+Compile: clang++ -std=c++11 counter_atomic.cpp -pthread
+
+Output: 5000
+
+*/
